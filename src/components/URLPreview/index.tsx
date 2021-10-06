09b726c23e55d97f1fa67b40185af8d2fe41fffd
0xtsukino <0xtsukino@gmail.com>
2021-10-07 00:56:34 +0800

    add regular link preview

--- src/components/URLPreview/index.tsx
+++ src/components/URLPreview/index.tsx
@@ -2,6 +2,7 @@ import React, {ReactElement, useCallback, useEffect, useState} from "react";
 import "./url-preview.scss";
 import Icon from "../Icon";
 import classNames from "classnames";
+import config from "../../util/config";
 
 type Props = {
     url?: string;
@@ -10,37 +11,92 @@ type Props = {
     onRemove?: () => void;
 };
 
+type Preview = {
+    link: string;
+    title: string;
+    description: string;
+    image: string;
+    mediaType: string;
+    contentType: string;
+    favicon: string;
+}
+
 export default function URLPreview(props: Props): ReactElement {
     const {url, editable} = props;
     const [imageSrc, setImageSrc] = useState('');
+    const [preview, setPreview] = useState<Preview|null>(null);
 
     useEffect(() => {
         (async function onURLPreviewLoad() {
-            if (!url) return;
+            if (!url) {
+                setPreview(null);
+                setImageSrc('');
+                return;
+            }
 
             try {
                 if (await testImage(url)) {
                     setImageSrc(url);
+                    return;
+                } else {
+                    setImageSrc('');
                 }
+
+                const resp = await fetch(`${config.indexerAPI}/preview?link=${encodeURI(url)}`);
+                const json = await resp.json();
+
+                if (!json.payload.error) {
+                    const {
+                        link,
+                        title = '',
+                        description = '',
+                        image = '',
+                        mediaType = '',
+                        contentType = '',
+                        favicon = '',
+                    } = json.payload;
+
+                    setPreview({
+                        link,
+                        title,
+                        description,
+                        image,
+                        mediaType,
+                        contentType,
+                        favicon,
+                    });
+
+                    return;
+                }
+
+                setPreview(null);
             } catch (e) {
                 setImageSrc('');
+                setPreview(null);
             }
 
 
         })();
-    }, [props.url]);
+    }, [url]);
 
     const openImageLink = useCallback((e) => {
         e.stopPropagation();
         window.open(imageSrc, '_blank');
     }, [imageSrc]);
 
-    if (!imageSrc) {
+    const openLink = useCallback((e) => {
+        if (!preview?.link) return;
+        e.stopPropagation();
+        window.open(preview?.link, '_blank');
+    }, [preview?.link]);
+
+    if (!imageSrc && !preview) {
         return <></>;
     }
 
     return (
         <div className="url-preview">
+
             { imageSrc && (
                 <div
                     className={classNames("url-preview__img-container", {
@@ -56,6 +112,26 @@ export default function URLPreview(props: Props): ReactElement {
                     />
                 </div>
             ) }
+
+            { preview && (
+                <div
+                    className={classNames("url-preview__link-container", {
+                        'cursor-pointer': !props.editable,
+                    })}
+                    onClick={!props.editable ? openLink : undefined}
+                >
+                    <div
+                        className={classNames("url-preview__link-image", {
+                        })}
+                        style={{backgroundImage: `url(${preview.image})`}}
+                    />
+                    <div className="url-preview__link-content">
+                        <div className="url-preview__link-title">{preview.title}</div>
+                        <div className="url-preview__link-desc">{preview.description}</div>
+                    </div>
+                </div>
+            ) }
+
             { editable && (
                 <Icon
                     className="url-preview__close bg-black bg-opacity-80 text-white absolute top-4 left-4 w-8 h-8"
@@ -76,19 +152,19 @@ function testImage(url: string) {
             // reset .src to invalid URL so it stops previous
             // loading, but doesn't trigger new load
             img.src = "//!!!!/test.jpg";
-            reject("timeout");
+            resolve(false);
         }, 5000);
 
         const img = new Image();
 
         img.onerror = img.onabort = function () {
             clearTimeout(timer);
-            reject("error");
+            resolve(false);
         };
 
         img.onload = function () {
             clearTimeout(timer);
-            resolve("success");
+            resolve(true);
         };
 
         img.src = url;
