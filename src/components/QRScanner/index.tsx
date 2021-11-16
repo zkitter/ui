d391c6cdec3e65b73caf2b8c5a450117a9bbcb36
0xtsukino <0xtsukino@gmail.com>
2021-11-16 20:23:47 +0800

    debug

--- src/components/QRScanner/index.tsx
+++ src/components/QRScanner/index.tsx
@@ -14,6 +14,7 @@ export default function QRScanner(): ReactElement {
 
         try {
             const identity: Identity = JSON.parse(data);
+            console.log(identity)
             await postWorkerMessage(setIdentity(identity));
             // await postWorkerMessage(selectIdentity(identity.publicKey));
         } catch (e) {
