673c6c9707eb16d4b5ca625b5950e76a4f9d2b5b
tsukino <87639218+0xtsukino@users.noreply.github.com>
2022-10-19 12:39:37 +0800

    fix: use correct proof type for TAZ file upload (#35)

--- src/util/merkle.ts
+++ src/util/merkle.ts
@@ -13,8 +13,10 @@ export type ValidGroups = 'zksocial_all'
 export const findProof = async (
     group: string,
     idCommitment: string,
+    proofType?: 'semaphore' | 'rln',
 ): Promise<MerkleProof & {group: string}|null> => {
-    const resp = await fetch(`${config.indexerAPI}/v1/proofs/${idCommitment}?group=${group}`);
+    const proofTypeParam = proofType ? `&proofType=${proofType}` : ''
+    const resp = await fetch(`${config.indexerAPI}/v1/proofs/${idCommitment}?group=${group}${proofTypeParam}`);
     const { payload, error } = await resp.json();
     const { data } = payload;
 
